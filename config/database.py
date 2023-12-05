05ed197d37613f7eddbe274e755607747e17e967
Ivo D'Ursi <ivomdursi@gmail.com>
2023-12-05 20:29:00 -0300

    Solved SSL Certificate

--- config/database.py
+++ config/database.py
@@ -1,13 +1,14 @@
 from pymongo.mongo_client import MongoClient
 from dotenv import load_dotenv
 import os
+import certifi
 
 load_dotenv()
 os.environ["MONGODB_CONN"]
 
 uri = os.environ["MONGODB_CONN"]
 
-client = MongoClient(uri)
+client = MongoClient(uri, tlsCAFile=certifi.where())
 
 db = client.prode
 
